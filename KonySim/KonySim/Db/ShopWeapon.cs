fd7ff918950bb4d40b502d0e5e8a0af461519e9f
BK-TN <me@bk-tn.com>
2016-03-16 13:20:37 +0100

    Created classes for all tables

---
+++ KonySim/KonySim/Db/ShopWeapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KonySim.Db
+{
+    internal class ShopWeapon : TableRow
+    {
+        public int ShopID { get; set; }
+        public int WeaponID { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ KonySim/KonySim/Db/Soldier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KonySim.Db
+{
+    internal class Soldier : TableRow
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Exp { get; set; }
+        public int Lvl { get; set; }
+        public int PortraitIndex { get; set; }
+        public int PlayerID { get; set; }
+        public int WeaponID { get; set; }
+    }
+}
