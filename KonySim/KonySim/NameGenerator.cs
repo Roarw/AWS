65f9840de057097e2e03b7369befbb93b5f8f10b
thomas <thomas.munk14@gmail.com>
2016-03-17 09:49:06 +0100

    commitment

---
+++ KonySim/KonySim/NameGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KonySim
+{
+    class NameGenerator
+    {
+       
+        private string[] names = new string[15] { "Luim", "aap", "swart", "poef", "speeksel", "lelike", "adebowale", "ayodele", "dubaku", "oog", "boemelaar", "jabari", "imamu", "john", "mugabe" };
+
+        Random random = new Random();
+        public string GetRandomName
+        {
+            get { return names [random.Next(strings.Length)]; }
+            
+        }
+
+    }
+}
