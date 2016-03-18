bd95814509141c4e9f7f7a5daf5de7fcc54a2c49
Roarw <zalwio@hotmail.dk>
2016-03-18 10:26:04 +0100

    Fkin shit

---
+++ KonySim/KonySim/UIListScrollerFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KonySim
+{
+    internal class UIListScrollerFactory : ButtonFactory, IUpdate
+    {
+        UIList uiList;
+        int factor;
+        bool mouseDown;
+
+        public UIListScrollerFactory(UIList uiList, int factor)
+        {
+            this.uiList = uiList;
+            this.factor = factor;
+            mouseDown = false;
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (mouseDown)
+            {
+                uiList.SetScrollerPosition(factor);
+            }
+        }
+
+        public void MouseEnter()
+        {
+            
+        }
+
+        public void MouseExit()
+        {
+            mouseDown = false;
+        }
+
+        public void MousePressed()
+        {
+            mouseDown = true;
+        }
+
+        public void MouseReleased()
+        {
+            mouseDown = false;
+        }
+    }
+}
